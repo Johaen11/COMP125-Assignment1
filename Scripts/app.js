41d10e889cb15a3bb4f41f67142a776a1a185df7
johaen11 <johngnanakumar@gmail.com>
2016-05-31 09:48:23 -0400

    Added Bootstrap, Jquery and font-awesome
    also added contents and scripts

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,4 +1,9 @@
 /* main JavaScript file */
+/*File Name: 
+* Author's Name: Johaen Gnanakumar
+* Website's Name:
+* File Description:
+*/
 // IIFE - Immediately Invoked Function Expression
 (function(){
     "use strict";
